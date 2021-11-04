42dba7fc8f8f5d5b13dd02f6aabfa06e258a170e
Alexamakans <fougner.alexander@gmail.com>
2021-11-04 11:31:25 +0100

    Initial commit

---
+++ client.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/Alexamakans/wharf-common-api-client/pkg/remoteprovider"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+// Client implements remoteprovider.Client.
+type Client struct {
+	remoteprovider.BaseClient
+}
+
+func (c *Client) FetchFile(projectIdentifier remoteprovider.ProjectIdentifier, fileName string) ([]byte, error) {
+	return []byte{}, nil
+}
+
+func (c *Client) FetchBranches(projectIdentifier remoteprovider.ProjectIdentifier) ([]remoteprovider.WharfBranch, error) {
+	return []remoteprovider.WharfBranch{}, nil
+}
+
+func (c *Client) FetchProjectByGroupAndProjectName(groupName, projectName string) (remoteprovider.WharfProject, error) {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: c.Token})
+	tc := oauth2.NewClient(c.Context, ts)
+	githubClient, err := github.NewEnterpriseClient(c.RemoteProviderURL, "", tc)
+	if err != nil {
+		return remoteprovider.WharfProject{}, fmt.Errorf("failed connecting to github using remote provider URL %q: %w", c.RemoteProviderURL, err)
+	}
+
+	githubRepo, _, err := githubClient.Repositories.Get(c.Context, groupName, projectName)
+	if err != nil {
+		return remoteprovider.WharfProject{}, fmt.Errorf("fetching github project '%s/%s' failed on %q: %w", groupName, projectName, c.RemoteProviderURL, err)
+	}
+
+	var project remoteprovider.WharfProject
+	project.RemoteProjectID = strconv.FormatInt(githubRepo.GetID(), 10)
+	project.GitURL = githubRepo.GetSSHURL()
+	project.Name = githubRepo.GetName()
+	project.GroupName = githubRepo.GetOwner().GetName()
+
+	return project, nil
+}
+
+func (c *Client) WharfProjectToIdentifier(project remoteprovider.WharfProject) remoteprovider.ProjectIdentifier {
+	return remoteprovider.ProjectIdentifier{
+		Values: []string{project.RemoteProjectID},
+	}
+}
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/Alexamakans/wharf-github-client
+
+go 1.17
+
+require (
+	github.com/Alexamakans/wharf-common-api-client v0.0.0-20211104102451-d53f3571b063
+	github.com/google/go-github v17.0.0+incompatible
+	golang.org/x/oauth2 v0.0.0-20211028175245-ba495a64dcb5
+)
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.7.4 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/iver-wharf/wharf-api v0.0.0-20211022062618-90bebcf4e5a1 // indirect
+	github.com/iver-wharf/wharf-core v1.2.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ugorji/go/codec v1.1.13 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/guregu/null.v4 v4.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
