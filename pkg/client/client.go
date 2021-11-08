dbc1539aa23ac377646c1274b32c3ed3e15c9af5
Alexamakans <fougner.alexander@gmail.com>
2021-11-08 15:31:48 +0100

    Changes

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,11 +1,13 @@
 package client
 
 import (
+	b64 "encoding/base64"
 	"fmt"
 	"strconv"
 
 	"github.com/Alexamakans/wharf-common-api-client/pkg/remoteprovider"
 	"github.com/google/go-github/github"
+	"github.com/iver-wharf/wharf-api/pkg/model/database"
 	"golang.org/x/oauth2"
 )
 
@@ -15,31 +17,73 @@ type Client struct {
 }
 
 func (c *Client) FetchFile(projectIdentifier remoteprovider.ProjectIdentifier, fileName string) ([]byte, error) {
-	return []byte{}, nil
+	githubClient, err := c.initGithubClient()
+	if err != nil {
+		return []byte{}, nil
+	}
+
+	repo, err := c.getRepo(githubClient, projectIdentifier.Values[0])
+	if err != nil {
+		return []byte{}, nil
+	}
+
+	fileContent, _, _, err := githubClient.Repositories.GetContents(c.Context, repo.GetOwner().GetName(), repo.GetName(), fileName, nil)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return b64.StdEncoding.DecodeString(*fileContent.Content)
 }
 
 func (c *Client) FetchBranches(projectIdentifier remoteprovider.ProjectIdentifier) ([]remoteprovider.WharfBranch, error) {
-	return []remoteprovider.WharfBranch{}, nil
+	githubClient, err := c.initGithubClient()
+	if err != nil {
+		return []remoteprovider.WharfBranch{}, nil
+	}
+
+	repo, err := c.getRepo(githubClient, projectIdentifier.Values[0])
+	if err != nil {
+		return []remoteprovider.WharfBranch{}, nil
+	}
+
+	branches, _, err := githubClient.Repositories.ListBranches(c.Context, repo.GetOwner().GetName(), repo.GetName(), nil)
+	if err != nil {
+		return []remoteprovider.WharfBranch{}, err
+	}
+
+	wharfBranches := make([]remoteprovider.WharfBranch, 0, len(branches))
+	for _, branch := range branches {
+		wharfBranches = append(wharfBranches, remoteprovider.WharfBranch{
+			Name:    branch.GetName(),
+			Default: branch.GetName() == repo.GetDefaultBranch()})
+		if err != nil {
+			break
+		}
+	}
+
+	return wharfBranches, nil
 }
 
 func (c *Client) FetchProjectByGroupAndProjectName(groupName, projectName string) (remoteprovider.WharfProject, error) {
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: c.Token})
-	tc := oauth2.NewClient(c.Context, ts)
-	githubClient, err := github.NewEnterpriseClient(c.RemoteProviderURL, "", tc)
+	githubClient, err := c.initGithubClient()
 	if err != nil {
-		return remoteprovider.WharfProject{}, fmt.Errorf("failed connecting to github using remote provider URL %q: %w", c.RemoteProviderURL, err)
+		return remoteprovider.WharfProject{}, nil
 	}
 
-	githubRepo, _, err := githubClient.Repositories.Get(c.Context, groupName, projectName)
+	repo, _, err := githubClient.Repositories.Get(c.Context, groupName, projectName)
 	if err != nil {
 		return remoteprovider.WharfProject{}, fmt.Errorf("fetching github project '%s/%s' failed on %q: %w", groupName, projectName, c.RemoteProviderURL, err)
 	}
 
-	var project remoteprovider.WharfProject
-	project.RemoteProjectID = strconv.FormatInt(githubRepo.GetID(), 10)
-	project.GitURL = githubRepo.GetSSHURL()
-	project.Name = githubRepo.GetName()
-	project.GroupName = githubRepo.GetOwner().GetName()
+	project := remoteprovider.WharfProject{
+		Project: database.Project{
+			Name:        repo.GetName(),
+			GroupName:   repo.GetOwner().GetLogin(),
+			Description: repo.GetDescription(),
+			AvatarURL:   *repo.GetOwner().AvatarURL,
+			GitURL:      *repo.GitURL,
+		},
+		RemoteProjectID: strconv.FormatInt(repo.GetID(), 10)}
 
 	return project, nil
 }
@@ -48,4 +92,29 @@ func (c *Client) WharfProjectToIdentifier(project remoteprovider.WharfProject) r
 	return remoteprovider.ProjectIdentifier{
 		Values: []string{project.RemoteProjectID},
 	}
+}
+
+func (c *Client) initGithubClient() (*github.Client, error) {
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: c.Token})
+	tc := oauth2.NewClient(c.Context, ts)
+	githubClient, err := github.NewEnterpriseClient(c.RemoteProviderURL, "", tc)
+	if err != nil {
+		return nil, fmt.Errorf("failed connecting to github using remote provider URL %q: %w", c.RemoteProviderURL, err)
+	}
+
+	return githubClient, nil
+}
+
+func (c *Client) getRepo(githubClient *github.Client, remoteProjectID string) (*github.Repository, error) {
+	id, err := strconv.ParseInt(remoteProjectID, 0, 64)
+	if err != nil {
+		return nil, fmt.Errorf("parsing remote project id failed %q: %w", remoteProjectID, err)
+	}
+
+	repo, _, err := githubClient.Repositories.GetByID(c.Context, id)
+	if err != nil {
+		return nil, fmt.Errorf("fetching github project with ID %q failed on %q: %w", remoteProjectID, c.RemoteProviderURL, err)
+	}
+
+	return repo, nil
 }
